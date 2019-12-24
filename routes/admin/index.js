d315c756fdb4a31408de7e08ef43882e7f70594a
serhii-hubchakevych <56347318+serhii-hubchakevych@users.noreply.github.com>
2019-12-24 14:48:57 +0200

    Added a Procfileaaaa.

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,28 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://serhii_hubchakevych:Lenovo1997@justnotescluster-7lzef.gcp.mongodb.net/JustNotes?retryWrites=true&w=majority"
 
 router.post('/', ( req, resp ) => {
-    const MongoClient = require('mongodb').MongoClient;
-    const uri = "mongodb+srv://serhii_hubchakevych:Lenovo1997@justnotescluster-7lzef.gcp.mongodb.net/JustNotes?retryWrites=true&w=majority"
     MongoClient.connect(uri, function(err, client) {
       if(err) {
           console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
       }
-      const collection = client.db("JustNotes").collection("AdminCollection");  
-      collection.findOne({login:req.body.login, password:req.body.password}, function(err, result) {
-            if (err) throw err;
-              if (result === null){
-                  resp.send(false)
-              } else {
-                const contentCollection = client.db("JustNotes").collection("ContentCollection");
-                contentCollection.find({}).toArray(function(err, result){
-                  if (err) throw err;
-                  resp.send(result[0])
-                })
-              }
-            client.close();
-          });
-        });
+      client.db("JustNotes").collection("AdminCollection").findOne({login:req.body.login, password:req.body.password}, function(err, result) {
+        if (err) throw err;
+          if (result === null){
+              resp.send(false)
+          } else {
+            client.db("JustNotes").collection("ContentCollection").find({}).toArray(function(err, result){
+              if (err) throw err;
+              resp.send(result[0])
+            })
+          }
+        client.close();
+      });
+    });
 })
 
 module.exports = router;
