7a22daaf375397d3fc47df76125a262f19318a1a
serhii-hubchakevych <56347318+serhii-hubchakevych@users.noreply.github.com>
2019-12-24 14:54:27 +0200

    Added a Procfile.aa

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json())
 app.use(function (req, res, next) {
 
     // Request methods you wish to allow
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     
@@ -20,10 +21,10 @@ app.use(function (req, res, next) {
     // Pass to next layer of middleware
     next();
 });
-app.use('/', (req, res, next)=>{
-    res.send('HELLO FROM HEROKU');
-    next();
-})
+// app.use('/', (req, res, next)=>{
+//     res.send('HELLO FROM HEROKU');
+//     next();
+// })
 app.use('/admin', admin);
 app.use('/just-notes-admin-panel', content);
 
