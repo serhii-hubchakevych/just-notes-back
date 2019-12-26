529053a4ae59ebfe5d1153f156a7fa52682df58b
serhii-hubchakevych <56347318+serhii-hubchakevych@users.noreply.github.com>
2019-12-26 15:24:34 +0200

    Added a Procfile.

--- routes/content/index.js
+++ routes/content/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://serhii_hubchakevych:Lenovo1997@justnotescluster-7lzef.gcp.mongodb.net/JustNotes?retryWrites=true&w=majority"
 
-router.put('/', ( req, res ) => {
+router.put('/', ( req, response ) => {
     MongoClient.connect(uri, function(err, client) {
       if(err) {
           console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
@@ -14,7 +14,7 @@ router.put('/', ( req, res ) => {
                   if (err) throw err;      
                   contentCollection.updateOne(result[0], myobj, function(err, res) {
                     if (err) throw err;
-                        res.send('Succes')
+                        response.send('Succes')
                     client.close();
                 });
             })
