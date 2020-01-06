6385a81f45a5fa5dc66b2f40c31e1eee7fd63a6b
serhii-hubchakevych <56347318+serhii-hubchakevych@users.noreply.github.com>
2020-01-06 11:36:25 +0200

    Added a Procfile.

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,21 +1,30 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://serhii_hubchakevych:Lenovo1997@justnotescluster-7lzef.gcp.mongodb.net/JustNotes?retryWrites=true&w=majority"
 
 router.post('/', ( req, res ) => {
+    const login = req.body.login;
     MongoClient.connect(uri, function(err, client) {
       if(err) {
-          console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+          console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
       }
       client.db("JustNotes").collection("AdminCollection").findOne({login:req.body.login, password:req.body.password}, function(err, result) {
         if (err) throw err;
           if (result === null){
-              res.send(false)
+              res.sendStatus(403)
           } else {
             client.db("JustNotes").collection("ContentCollection").find({}).toArray(function(err, result){
               if (err) throw err;
-              res.send(result[0])
+              const resultWithToken = result;
+              jwt.sign({ login: login }, 'secretkey', (err, token)=> {
+                if(err) {
+                  console.log('Error occurred while generating token...\n', err);
+                }
+                resultWithToken.push(token);
+                res.send(resultWithToken)
+              })
             })
           }
         client.close();
--- routes/content/index.js
+++ routes/content/index.js
@@ -1,24 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://serhii_hubchakevych:Lenovo1997@justnotescluster-7lzef.gcp.mongodb.net/JustNotes?retryWrites=true&w=majority"
 
-router.put('/', ( req, response ) => {
-    MongoClient.connect(uri, function(err, client) {
-      if(err) {
-          console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+router.put('/', verifyToken, ( req, response ) => {
+    const updateData = { $set: req.body };
+    jwt.verify(req.token, 'secretkey', (err)=> {
+      if(err){
+        response.sendStatus(403);
+      } else {
+        MongoClient.connect(uri, function(err, client) {
+          if(err) {
+              console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+          }
+          const contentCollection = client.db("JustNotes").collection("ContentCollection");
+                    contentCollection.find({}).toArray(function(err, result){
+                      if (err) throw err;      
+                      contentCollection.updateOne(result[0], updateData, function(err, res) {
+                        if (err) throw err;
+                            response.sendStatus(200)
+                        client.close();
+                    });
+                })
+        });
       }
-      let myobj = { $set: req.body };
-      const contentCollection = client.db("JustNotes").collection("ContentCollection");
-                contentCollection.find({}).toArray(function(err, result){
-                  if (err) throw err;      
-                  contentCollection.updateOne(result[0], myobj, function(err, res) {
-                    if (err) throw err;
-                        response.send('Succes')
-                    client.close();
-                });
-            })
-    });
+    }) 
+ 
 })
 router.get('/', ( req, res ) => {
     MongoClient.connect(uri, function(err, client) {
@@ -30,5 +38,20 @@ router.get('/', ( req, res ) => {
         res.send(result[0])
       })
     })
-  })
+})
+
+function verifyToken( req, res, next ){
+  const bearerHeader = req.headers['authorization']
+
+  if ( typeof bearerHeader !== 'undefined' ){
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+
+}
+
 module.exports = router;
